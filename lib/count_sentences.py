bb7f31b3a527a7e72761e30603a0279cd9059dd0
Michael Alan Stowe <mastowe94@gmail.com>
2023-11-11 18:04:16 -0600

    ugly count_sentences but works at the moment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+
+  def __init__(self,value = ''):
+    if isinstance(value,str) and len(value) != 0:
+      self.value = value
+    else:
+      self.value = ''
+      print("The value must be a string.")
+  
+  def is_question(self):
+    return self.value.endswith("?")
+  
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def is_sentence(self):
+    return self.value.endswith(".")
+  
+  def count_sentences(self):
+    if len(self.value) == 0:
+      return 0
+    else:
+      return len(self.value.replace("!",".").replace("?",".").split(". "))
+
+
